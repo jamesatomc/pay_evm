91f97af2bd38e244044c6d23fcfcbe085c771c86
jamesatomc <jiraphonwansai2002@gmail.com>
2025-08-21 20:55:25 +0700

    feat: Update Android SDK versions and enable immersive fullscreen mode in main.dart

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,8 @@ plugins {
 
 android {
     namespace = "site.kanari.kanaripay"
-    compileSdk = flutter.compileSdkVersion
+    // Use explicit SDK versions to ensure newer Android attributes (eg. allowTaskSnapshot) are available
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -24,8 +25,9 @@ android {
         applicationId = "site.kanari.kanaripay"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+    minSdk = flutter.minSdkVersion
+    // Explicit targetSdk to match compileSdk and enable newer platform attributes
+    targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
